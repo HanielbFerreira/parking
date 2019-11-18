55542ff3e9527b0cd2926e7a9a366652abbaf8ef
Ferreira, Haniel B <haniel.b.ferreira@accenture.com>
2019-11-18 14:57:15 -0300

    show

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 class Database {
 
-    private DB_URI = 'mongodb://mongodb:27017/park';
+    private DB_URI = 'mongodb://root:root@mongodb:27017/park';
 
     private DB_CONNECTION: any;
 
