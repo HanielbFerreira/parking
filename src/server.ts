42e9ecc2e4a836dacbabd599576ceda1b03fcf98
Ferreira, Haniel B <haniel.b.ferreira@accenture.com>
2019-11-19 17:13:58 -0300

    go

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,6 @@ import App from './app';
 
 const port = 4000;
 
-process.once('SIGUSR2', () => App.closeDataBaseConnection('nodemon restart', ()=> process.kill(process.pid, 'SIGUSR2')));
-process.on('SIGINT', () => App.closeDataBaseConnection('execução foi interrompida', ()=> process.exit(0)));
-
 App.app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
 })
